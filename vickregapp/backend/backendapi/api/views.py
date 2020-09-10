1a16cd9a2e34d2828bf67dba2ee42765e7a3de7e
Chinedu Victor <nwachukwuvictorc@gmil.com>
2020-09-10 03:52:29 +0100

    vicregapp

---
+++ vickregapp/backend/backendapi/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from api.serializers import UserSerializer, BookSerializer
+from .models import Book
+
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
+
+
+
